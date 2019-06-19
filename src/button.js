403ddf1b5198e3cd0df4f5dfd8c6acf1ff6f386a
galtnie <hereby@protonmail.com>
2019-06-19 15:52:10 +0300

    completed debounce on button clicking

--- src/button.js
+++ src/button.js
@@ -2,7 +2,19 @@ import React, { useState, useEffect } from "react";
 
 const Button = () => {
   const [input, setInput] = useState("");
-  const [text, setText] = useState("");
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("");
+  const [click, setClick] = useState(0);
+
+  const finalValue1 = useDebounce1(input, 1000, click);
+
+  useEffect(() => {
+    if (click > 0) {
+      setText1(finalValue1);
+    } else {
+      setText1("");
+    }
+  }, [finalValue1, click]);
 
   return (
     <div>
@@ -13,28 +25,34 @@ const Button = () => {
           setInput(e.target.value);
         }}
       />
-      <button onClick={() => setText(input)}>appear</button>
-      <b>text will appear in 2 secs unless you click again</b>
+      <button onClick={() => setClick(click + 1)}>appear</button>
+      <b>
+        when you click the button the text will appear in a sec unless you click
+        again
+      </b>
       <br />
       text invoked by button click:
-      <span> {text}</span>
+      <span> {text1}</span>
+      <br />
+      text invoked by typing stop:
+      <span> {} </span>
     </div>
   );
 };
 
-// function useDebounce(value, wait) {
-//   const [debouncedValue, setDebouncedValue] = useState(value);
+function useDebounce1(value, wait, click) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
-//   useEffect(() => {
-//     const watcher = setTimeout(() => {
-//       setDebouncedValue(value);
-//     }, wait);
-//     return () => {
-//       clearTimeout(watcher);
-//     };
-//   }, [value, wait]);
+  useEffect(() => {
+    const watcher = setTimeout(() => {
+      setDebouncedValue(value);
+    }, wait);
+    return () => {
+      clearTimeout(watcher);
+    };
+  }, [value, wait, click]);
 
-//   return(debouncedValue);
-// }
+  return debouncedValue;
+}
 
 export default Button;
